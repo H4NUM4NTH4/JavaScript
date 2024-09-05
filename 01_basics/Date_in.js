90f3473a5bea2b6add58c7a29d8975f6a1992a44
H4NUM4NTH4 <154608284+H4NUM4NTH4@users.noreply.github.com>
2024-09-05 05:21:43 +0000

    Dates

---
+++ 01_basics/Date_in.js
@@ -0,0 +1,13 @@
+//Dates
+
+let myDate = new Date()
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleString())
+
+
+let myCreatedDate = new Date("2024-09-05")
+console.log(myCreatedDate.toLocaleString())
+
+let newDay = new Date()
+console.log(newDay.getDate())
+console.log(newDay.getMonth())
